3b261f2fa52ad269b02bdccbc68cc51daaef9207
Edzel <derz@elidragon.com>
2021-11-15 08:37:07 +0100

    yay fixed UA

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if(str_replace("curl/", "", $_SERVER['HTTP_USER_AGENT']) !== $_SERVER['HTTP_USER_AGENT']) {
+if(str_replace("curl/", "", $_SERVER['HTTP_USER_AGENT']) !== $_SERVER['HTTP_USER_AGENT'] or $_SERVER["HTTP_HOST"] === "sh.derzombiiie.com") {
 	die(file_get_contents("../sh.derz/derz.sh"));
 }
 ?>
