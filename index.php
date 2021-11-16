a856c9cb545dfbe40963c11b50cfc1e237259f2b
Edzel <derz@elidragon.com>
2021-11-16 09:48:20 +0100

    text/plain content type

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 if(str_replace("curl/", "", $_SERVER['HTTP_USER_AGENT']) !== $_SERVER['HTTP_USER_AGENT'] or $_SERVER["HTTP_HOST"] === "sh.derzombiiie.com") {
+	header("Content-Type: text/plain");
 	die(file_get_contents("../sh.derz/derz.sh"));
 }
 ?>
